54bb16212298b6f296eeb261bb1eeab793afa6e3
Mario <albeto8@gmail.com>
2018-08-21 13:01:35 -0500

    Consuming actions in reducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,19 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          onClick={() => this.props.selectBook(book)}
+          key={book.title}
+          className="list-group-item"
+        >
+          {book.title}
+        </li>
       )
     })
   }
@@ -23,4 +31,8 @@ const mapStateToProps = (state) => {
   return { books: state.books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,7 @@
+export function (state = null, action) {
+  switch (expression) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  return state
+}
